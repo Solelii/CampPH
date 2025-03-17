cfd99d12ceb202fea6503b7426b6ca35d0cb1d8d
Seol <khurtdilanco28@gmail.com>
2025-03-18 02:32:01 +0800

    COMMIT: campgroundsheet icon attributes

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class CampgroundStrings {
+  static const String about = "About";
+  static const String review = "Review";
+  static const String features = "Features";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camph/campgroundsheet.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -85,30 +86,34 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SizedBox.expand(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            // CampgroundSheet(
+            //   name: '', 
+            //   isPublic: true, 
+            //   rating: 5.0, 
+            //   location: '', 
+            //   socialMediaLink: '',
+            //   phoneNumber: '', 
+            //   firePit: true, 
+            //   restroom: true, 
+            //   petsAllowed: true, 
+            //   picnicTable: true, 
+            //   signal: true, 
+            //   shower: true, 
+            //   tapWater: true, 
+            //   firstAid: true, 
+            //   security: true, 
+            //   hikingTrails: true, 
+            //   bikingTrails: true, 
+            //   store: true, 
+            //   foodServices: true, 
+            //   lakesOrRiver: true, 
+            //   electricity: true, 
+            //   wifi: true,
+            // )
           ],
         ),
       ),
